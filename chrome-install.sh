9a46d14b5694fd60d8aa4c7aa2aee9a23bbee114
Will <will62185@gmail.com>
2018-06-19 23:03:10 -0400

    added two scripts

---
+++ chrome-install.sh
@@ -0,0 +1,13 @@
+# Script to install Chrome
+
+echo "Starting Install . . ."
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O chrome_amd64.deb
+
+sudo dpkg -i chrome_amd64.deb
+
+rm chrome_amd64.deb
+
+echo "Install Finished"
+
+
---
+++ postman-install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#---Script to install Postman---#
+wget https://dl.pstmn.io/download/latest/linux64 -O postman.tar.gz
+sudo tar -xzf postman.tar.gz -C /opt
+sudo ln -s /opt/Postman/Postman /usr/bin/postman
+
+#Create a Desktop Entry
+cat > ~/.local/share/applications/postman.desktop <<EOL
+[Desktop Entry]
+Encoding=UTF-8
+Name=Postman
+Exec=postman
+Icon=/opt/Postman/app/resources/app/assets/icon.png
+Terminal=false
+Type=Application
+Categories=Development;
+EOL
+rm postman.tar.gz
+echo "Completed"
